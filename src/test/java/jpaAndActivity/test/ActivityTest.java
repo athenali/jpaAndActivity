14ab48ac99fb4a723fe7e87d5471e228820aac22
athena.li <xiaonan_li03@163.com>
2017-12-18 15:23:18 +0800

    驳回和待办

--- src/test/java/jpaAndActivity/test/ActivityTest.java
+++ src/test/java/jpaAndActivity/test/ActivityTest.java
@@ -1,19 +1,24 @@
 package jpaAndActivity.test;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
+import org.activiti.engine.HistoryService;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
-import org.activiti.engine.impl.cmd.DeleteProcessInstanceCmd;
-import org.activiti.engine.runtime.Execution;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.history.HistoricTaskInstanceQuery;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.jpaAndActivity.dto.leaveProject;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mvc.xml" })
@@ -24,9 +29,17 @@ private	ProcessEngine processEngine;
 @Test
 public void StartProcess() {
 	//提交申请
-	String initiator="lichuang";
+	String initiator="lichuang";//申请人
+	   List<leaveProject> leaveProjects=new ArrayList<>();//审批人
+	   leaveProject leave=new leaveProject();
+	   leave.setUserName("zhangsan");
+	   leaveProjects.add(leave);
+	   leaveProject leave1=new leaveProject();
+	   leave1.setUserName("lisi");
+	   leaveProjects.add(leave1);
 	Map<String, Object> map=new HashMap<>();
 	map.put("initiator", initiator);
+	map.put("leaveProjects", leaveProjects);
 	RuntimeService runtimeService = processEngine.getRuntimeService();
 	/**
 	 * leave :流程图中整个流程的id
@@ -54,6 +67,66 @@ public void deleteProcess() {
 		processEngine.getRuntimeService().deleteProcessInstance(processInstance.getId(), "withdraw");
 	System.out.println(processInstance.getBusinessKey()+processInstance.getProcessInstanceId());
    }
-
-
+//根据任务id获取businesskey
+@Test
+public void getBusinessKey() {
+	String taskId="10008";
+	HistoryService historyService = processEngine.getHistoryService();
+	String processInstanceId = historyService.createHistoricTaskInstanceQuery().taskId(taskId).singleResult()
+			.getProcessInstanceId();
+	String businessKey = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId)
+			.singleResult().getBusinessKey();
+	System.out.println(businessKey);
+}
+//审批   并添加审批意见,这里是审批通过
+@Test
+public void complete() {
+String taskId="12528";
+String comment="审批意见"; //在表  ACT_HI_COMMENT
+TaskService taskService = processEngine.getTaskService();
+Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+String processInstanceId = task.getProcessInstanceId();
+taskService.addComment(taskId, processInstanceId, comment);//添加审批意见
+taskService.complete(taskId);
+  }
+//审批，驳回申请
+//和撤销申请一样，需要重新提交申请
+@Test
+public  void  reject() {
+	String  taskId="12521";
+	String comment="审批不通过"; //在表  ACT_HI_COMMENT
+	TaskService taskService = processEngine.getTaskService();
+	Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+	String processInstanceId = task.getProcessInstanceId();
+	taskService.addComment(taskId, processInstanceId, comment);//添加审批意见
+	processEngine.getRuntimeService().deleteProcessInstance(processInstanceId, "reject");
+	System.out.println("申请被驳回");
+}
+/**
+ * 待办审批
+ */
+@Test
+public  void  needComplete() {
+	//List<Task> list = processEngine.getTaskService().createTaskQuery().taskAssignee("lichuang").list();
+	TaskQuery taskQuery = processEngine.getTaskService().createTaskQuery().orderByTaskCreateTime().desc();
+	List<Task> listPage = taskQuery.listPage(0, 10);//分页
+	taskQuery.count();//总数
+	for (Task task : listPage) {
+		System.out.println(task.getAssignee()+"============="+task.getProcessDefinitionId());
+	}
+	System.out.println("待办的总数"+taskQuery.count());
+}
+/**
+ * 已经办理的任务
+ */
+@Test
+public void  exitComplete() {
+	HistoricTaskInstanceQuery desc = processEngine.getHistoryService().createHistoricTaskInstanceQuery()
+			.finished().taskDeleteReason("completed").orderByTaskCreateTime().desc();
+	List<HistoricTaskInstance> listPage = desc.listPage(0, 10);//分页
+	for (HistoricTaskInstance historicTaskInstance : listPage) {
+		System.out.println(historicTaskInstance.getAssignee()+"===="+historicTaskInstance.getProcessInstanceId());
+	}
+	System.out.println("已经办理任务的总数"+desc.count());
+}
 }
